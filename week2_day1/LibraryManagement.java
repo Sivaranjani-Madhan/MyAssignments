547dbc73e5364676ebc5346a2bcf2e7e872e25cc
Sivaranjani-Madhan <sivu369@gmail.com>
2025-06-03 07:45:11 +0530

    Add files via upload
    week2_day1

---
+++ week2_day1/LibraryManagement.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Library options=new Library();
+		options.addBook("java");
+		options.issueBook();
+
+	}
+
+}
