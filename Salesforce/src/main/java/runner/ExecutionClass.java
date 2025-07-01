79d0f2920ea76c156170435667a25bb34a5734fb
Sivaranjani-Madhan <sivu369@gmail.com>
2025-07-01 17:29:21 +0530

    Delete Salesforce/src/main/java directory

--- Salesforce/src/main/java/runner/ExecutionClass.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src\\main\\java\\features\\LoginAccount.feature",glue="steps",monochrome=true,publish=true )
-public class ExecutionClass extends AbstractTestNGCucumberTests {
-
-}
